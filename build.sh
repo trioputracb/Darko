be2ab5e95c2eb37254058cc9e38ebe04430871b3
Trio Putra C B <trioputracb@Trios-MacBook-Pro-M1-2.local>
2026-08-01 08:25:43 +0700

    feat: add app icon and show it in readme

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ swift build -c release
 
 APP="dist/Darko.app"
 rm -rf "$APP"
-mkdir -p "$APP/Contents/MacOS"
+mkdir -p "$APP/Contents/MacOS" "$APP/Contents/Resources"
 
 cp ".build/release/Darko" "$APP/Contents/MacOS/Darko"
 cp "Sources/Darko/Info.plist" "$APP/Contents/Info.plist"
+cp -R "Sources/Darko/Resources/Darko.icon" "$APP/Contents/Resources/Darko.icon"
+cp "Sources/Darko/Resources/Darko.icns" "$APP/Contents/Resources/Darko.icns"
 
 codesign --force --deep --sign - "$APP"
 
